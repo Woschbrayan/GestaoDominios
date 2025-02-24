e48771a7d964ff37ad46c20cfd38faefb8dabfb7
Woschbrayan <brayanwosch@gmail.com>
2025-02-24 14:46:41 -0300

    Atualização do projeto

--- resources/views/domains/edit.blade.php
+++ resources/views/domains/edit.blade.php
@@ -1,5 +1,5 @@
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Novo Domínios</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
@@ -8,14 +8,14 @@
 <div class="container">
     <h2>Editar Domínio</h2>
     @if($errors->any())
-    <div class="alert alert-danger">
-        <ul type="none">
-            @foreach($errors->all() as $error)
-               <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+        <div class="alert alert-danger">
+            <ul type="none">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <form method="POST" action="/domains/{{ $domain->id }}">
         @csrf
